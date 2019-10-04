0a40cf89794c3ce1c8a5b924d3b076da19943280
Julio Noguera <jnoguera@smarttaxi.co>
2019-10-03 22:06:16 -0500

    new

---
+++ src/pages/Login/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login(){
+
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event){
+      event.preventDefault();
+  
+      const response = await api.post('/sessions', {email});
+  
+      const { _id } = response.data;
+  
+      localStorage.setItem('user', _id);
+    }
+
+    return (
+        <>
+            <p>
+            Ofrezca <strong>spots</strong> para programadores y encuentre <strong>talentos</strong> para su empresa.
+            </p>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-MAIL *</label>
+            <input  type="email" 
+                    id="email" 
+                    placeholder="Inserte su email"
+                    value={email}
+                    onChange={event => setEmail(event.target.value) }></input>
+            <button type="Submit" className="btn">Entrar</button>
+            </form>
+        </>
+    )
+}
