35b7680c18b0e9eb4d9cbfe2b60ae6dcd9c4850b
Julio Noguera <jnoguera@smarttaxi.co>
2019-10-03 21:25:26 -0500

    changes in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
+
 import './App.css';
 import logo from './assets/logo.svg';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {email});
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="logo"/>
@@ -12,12 +27,14 @@ function App() {
            Ofrezca <strong>spots</strong> para programadores y encuentre <strong>talentos</strong> para su empresa.
         </p>
 
-        <form action="">
-          <label htmlFor="email">mail</label>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
           <input  type="email" 
                   id="email" 
-                  placeholder="Inserte su email"/>
-          <input type="Submit"/>Entrar
+                  placeholder="Inserte su email"
+                  value={email}
+                  onChange={event => setEmail(event.target.value) }></input>
+          <button type="Submit" className="btn">Entrar</button>
         </form>
       </div>
     </div>
