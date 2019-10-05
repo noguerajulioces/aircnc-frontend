7a45c6d38d237f07f89d6f0211bf64acf0710cd8
Julio Noguera <noguerajulio24@gmail.com>
2019-10-04 23:46:14 -0500

    add css for new

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,24 +1,44 @@
 import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 
 import camera from '../../assets/camera.svg';
 
 import './style.css';
 
-export default function New(){
+export default function New({history}){
     const [tech, setTechs] = useState('');
     const [company, setCompany] = useState('');
     const [price, setPrice] = useState('');
-
     const [thumbnail, setThumbnail] = useState(null);
 
-    function handleSubmit(){
+    const preview = useMemo(() => {return thumbnail ? URL.createObjectURL(thumbnail) : null; },
+        [thumbnail]
+    )
 
+    async function handleSubmit(event){
+
+        event.preventDefault();
+
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('tech', tech);
+        data.append('price', price);
+
+        await api.post('/spots', data, {
+            headers: { user_id }
+        })
+
+        history.push('/dashboard');
     }
 
     return (
         <>
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail">
+            <label id="thumbnail" style={{ backgroundImage: `url(${preview})`}}
+            className={thumbnail ? 'has-thumbnail': ''}>
                 <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
                 <img src={camera} alt="Select image"></img>
             </label>
