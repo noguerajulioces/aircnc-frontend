b19dfc7ed40ebbaa5ce9a3b2bbbfe7cb960b248f
Julio Noguera <noguerajulio24@gmail.com>
2019-10-04 23:32:23 -0500

    add style

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+
+import camera from '../../assets/camera.svg';
+
+import './style.css';
 
 export default function New(){
-    return <div />
+    const [tech, setTechs] = useState('');
+    const [company, setCompany] = useState('');
+    const [price, setPrice] = useState('');
+
+    const [thumbnail, setThumbnail] = useState(null);
+
+    function handleSubmit(){
+
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <label id="thumbnail">
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+                <img src={camera} alt="Select image"></img>
+            </label>
+        </form>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="company">Empresa *</label>
+            <input
+            id="company"
+            placeholder="Ingrese su empresa"
+            value={company}
+            onChange={event => setCompany(event.target.value)} />
+            
+            <label htmlFor="tech">Tecnologías * <span>(separadas por comas)</span></label>
+            <input 
+                id="tech"
+                placeholder="Cuales son sus tecnologías"
+                value={tech}
+                onChange={event => setTechs(event.target.value)} 
+            />
+
+            <label htmlFor="price">Valor por día * <span>(deja en blanco para gratuito)</span></label>
+            <input 
+                id="price"
+                placeholder="Cuanto cobra por día"
+                value={price}
+                onChange={event => setPrice(event.target.value)} 
+            />
+
+            <button type="submit" className="btn">Guardar</button>
+        </form>
+        </>
+    )
 }
